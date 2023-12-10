67e5d7b32f69c7e29c7d1951876377d9a82ae842
ShowWoodenHorses <89218452+ShowWoodenHorses@users.noreply.github.com>
2023-12-10 21:54:34 +0300

    upd Building

---
+++ Assets/Scripts/Player/SetMaterialToInvisible.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class SetMaterialToInvisible : MonoBehaviour 
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+
+        }
+        
+    }
+}
